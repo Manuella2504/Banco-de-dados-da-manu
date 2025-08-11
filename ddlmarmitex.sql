c4cea124de8f492ee961b1690629d053037b066f
Manuella <manuella.rosario@ba.estudante.senai.br>
2025-08-11 08:07:15 -0300

    Update ddlmarmitex.sql

--- ddlmarmitex.sql
+++ ddlmarmitex.sql
@@ -1,25 +1,6 @@
 CREATE DATABASE Marmitex;
 USE marmitex;
 
-create table login(
-id integer not null,
-email varchar(255) not null,
-senha varchar(255) not null,
-nivel_acesso integer not null
-,
-primary key(id)
-);
-
-create table pedido(
-id integer not null,
-telefone_cliente varchar(255) not null,
-id_entregador integer not null,
-status varchar(255) not null,
-taxa_entrega varchar(255),
-data_pedido datetime,
-primary key(id)
-);
-
 create table cliente(
 telefone varchar(255),
 nome varchar(255),
@@ -28,21 +9,62 @@ pontoreferencia varchar(255),
 primary key(telefone)
 );
 
-create table item_pedido(
-id_marmita integer,
-id_pedido integer,
-quantidade integer
+create table login(
+id integer not null,
+email varchar(255) not null,
+senha varchar(255) not null,
+nivel_acesso integer not null,
+primary key(id)
+); 
+
+create table pedido(
+id integer not null,
+telefone_cliente varchar(255) not null,
+id_entregador integer not null,
+status varchar(255) not null,
+taxa_entrega varchar(255),
+data_pedido datetime,
+primary key(id),
+constraint fk_pedido_cliente foreign key(telefone_cliente) references cliente(telefone)
 );
 
 create table marmita(
-id integer,
-nome varchar(255),
-descricao mediumtext,
+id integer not null,
+nome varchar(255) not null,
+descricao mediumtext not null,
+primary key(id)
+);
+
+create table item_pedido(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_marmita integer not null,
+    id_pedido integer not null,
+    quantidade integer not null,
+    constraint fk_item_pedido_marmita foreign key(id_marmita) references marmita(id),
+    constraint fk_item_pedido_pedido foreign key(id_pedido) references pedido(id)
+);
+
+
+create table empresa(
+id integer not null,
+nome varchar(255) not null,
+cnpj varchar(255) not null,
+telefone varchar(255) not null,
+endereco varchar(255) not null,
 primary key(id)
 );
 
 create table entregador(
-id integer
-)
-create table empresa()
+id integer not null,
+id_empresa integer not null,
+nome varchar(255) not null,
+cpf varchar(255),
+celular varchar(255),
+primary key(id),
+constraint fk_entregador_empresa foreign key(id_empresa) references empresa(id)
+);
+
+
+
+
 
